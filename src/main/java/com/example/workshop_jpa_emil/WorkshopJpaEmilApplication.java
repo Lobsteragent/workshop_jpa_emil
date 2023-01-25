e28571d8e2d2b359cd1abdee5ab59f0ebea3483c
Emil Rutberg <Emil_skull@hotmail.com>
2023-01-25 09:47:46 +0100

    Initial commit

---
+++ src/main/java/com/example/workshop_jpa_emil/WorkshopJpaEmilApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshop_jpa_emil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJpaEmilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopJpaEmilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workshop_jpa_emil/WorkshopJpaEmilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop_jpa_emil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaEmilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
